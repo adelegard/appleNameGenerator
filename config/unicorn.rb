c778340d5138e6da0395c3667ad000e6eb35e526
Alex Delegard <adelegard@gmail.com>
2012-06-10 23:54:06 -0500

    Setting unicorn workers to 4 (testing).

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes 2 # amount of unicorn workers to spin up
+worker_processes 4 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
 
