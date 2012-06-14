93336cb722e8704fbd8e1ea7ae72b330b83e7894
Alex Delegard <adelegard@gmail.com>
2012-06-13 22:39:53 -0500

    Adding meta data to head to improve google searching.

--- config/environments/staging.rb
+++
@@ -1,7 +0,0 @@
-MyApp::Application.configure do
-  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
-    [u, p] == ['username', 'password']
-  end
-
-  #... other config
-end
